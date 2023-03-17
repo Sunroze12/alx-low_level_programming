a30a614737a35ba562d67c0f70fcc1be00fe2a36
Sunroze12 <fatimamakdad070@gmail.com>
2023-03-16 19:42:42 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
-* main - Prints a quotein the standard error.
+* main - Entry point of the program
 * Return: 1(success)
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	fputs("and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", stderr);
 	return (1);
 }
+
 
