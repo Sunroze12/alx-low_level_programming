8b1c6fbd8ff8100d338af82f98fdb596de3e8862
Sunroze12 <fatimamakdad070@gmail.com>
2023-03-17 13:08:02 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ *
+ * Return: alzays 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+	for (i = 65; i <= 90; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
